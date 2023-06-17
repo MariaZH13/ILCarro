79cd50a838c78fccd682eeeef36c1cb6285f19e9
MariaZH13 <134274880+MariaZH13@users.noreply.github.com>
2023-06-17 19:17:54 +0300

    HW_7(17.05)

---
+++ src/main/java/manager/HelperUser.java
@@ -0,0 +1,32 @@
+package manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperUser extends HelperBase{
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+
+    public void openLoginForm(){
+        wd.findElement(By.xpath("//a[.=' Log in ']")).click();
+    }
+    public void fillLoginForm(String email, String password){
+        type(By.id("email"),email);
+        type(By.id("password"),password);
+    }
+
+    public void submitLogin(){
+
+        click(By.cssSelector("button[type='submit"));
+
+    }
+
+    public void logOut(){
+        click(By.xpath("//a[.=' Logout ']"));
+
+    }
+    public boolean isLogged(){
+        return isElementPresent(By.xpath("//a[.=' Logout ']"));
+    }
+}
---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+
+
+
+    @BeforeMethod
+    public void precondition(){
+        if(app.getUser().isLogged()){
+            app.getUser().logOut();
+        }
+    }
+
+    @Test
+    public void loginPositive(){
+        String email = "marzh@gmail.com", password = "Qwert123$";
+        app.getUser().openLoginForm();
+        app.getUser().fillLoginForm(email,password);
+        app.getUser().submitLogin();
+       // app.getUser().pause(3000);
+        Assert.assertTrue(app.getUser().isElementPresent(By.cssSelector("button[type='button']")));
+
+    }
+
+
+}
