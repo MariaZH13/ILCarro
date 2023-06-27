e8eeca76f1edf92df3dabf9fed255dd4b3235894
MariaZH13 <134274880+MariaZH13@users.noreply.github.com>
2023-06-27 20:27:05 +0300

    ClassWork_10

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -2,7 +2,12 @@ package manager;
 
 import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
@@ -31,13 +36,35 @@ public class HelperUser extends HelperBase{
         type(By.id("lastName"),user.getLastName());
         type(By.id("email"),user.getEmail());
         type(By.id("password"),user.getPassword());
-        click(By.cssSelector("label[for='terms-of-use']"));
+        clickCheckbox();
     }
 
     // method signature - type + name + parameters types
 
+
+    public void clickCheckbox(){
+        System.out.println("Clicked Checkbox");
+ //     variant 1
+ //       click(By.cssSelector("label[for='terms-of-use']"));
+//      variant 2
+//        JavascriptExecutor js = (JavascriptExecutor) wd;
+//        js.executeScript("document.querySelector('#terms-of-use').click()");
+ //     variant 3
+        Rectangle rect = wd.findElement(By.cssSelector("div.checkbox-container")).getRect();
+        int x = rect.getX() + 5;
+        int y = rect.getY() + rect.getHeight() / 4;
+        Actions actions = new Actions(wd);
+        actions.moveByOffset(x,y).click().perform();
+
+
+
+
+
+    }
+
     public void submitLoginYalla(){
        wd.findElement(By.cssSelector("button[type='submit']")).submit();
+ //      wd.findElement(By.cssSelector("button[type='submit']")).click();
 
     }
 
@@ -64,9 +91,9 @@ public class HelperUser extends HelperBase{
     }
 
     public boolean isRegistrationUnsuccessful(){
+       
         return isElementPresent(By.xpath("//div[@class='error']"));
     }
-
 
 
 }
--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -4,23 +4,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistrationTests extends TestBase{
+public class RegistrationTests extends TestBase {
 
     @BeforeMethod
-    public void precondition(){
+    public void precondition() {
 
-        if(app.getUser().isLogged()){
+        if (app.getUser().isLogged()) {
             app.getUser().logOut();
         }
     }
 
     @Test
-    public void registrationPositive(){
-        int i = (int)(System.currentTimeMillis()/1000)%3600;
+    public void registrationPositive() {
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
         User user = User.builder()
                 .name("Bobbi")
                 .lastName("Brown")
-                .email("bobb" + i +"@com.com")
+                .email("bobb" + i + "@com.com")
                 .password("Qwert123$")
                 .build();
 //                .withName("Bobbi")
@@ -36,42 +36,61 @@ public class RegistrationTests extends TestBase{
     }
 
     @Test
-    public void registrationNegativePassword(){
-        int i = (int)(System.currentTimeMillis()/1000)%3600;
+    public void registrationNegativeWrongPassWord() {
+        int i = (int) (System.currentTimeMillis() / 1000) % 3600;
         User user = User.builder()
-                .name("Dana")
-                .lastName("Banana")
-                .email("fruit"+ i +"@gmail.com")
-                .password("Qwerty123")
+                .name("Bobbi")
+                .lastName("Brown")
+                .email("bobb" + i + "@com.com")
+                .password("Qwert123")
                 .build();
+
         app.getUser().openRegistrationForm();
         app.getUser().fillRegistrationForm(user);
         app.getUser().submitLoginYalla();
-        app.getUser().pause(3000);
-        Assert.assertTrue(app.getUser().isRegistrationUnsuccessful());
+//        app.getUser().pause(3000);
+//        Assert.assertTrue(app.getUser().isLoggedSuccessful());
+    }
+        @Test
+        public void registrationNegativePassword () {
+            int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+            User user = User.builder()
+                    .name("Dana")
+                    .lastName("Banana")
+                    .email("fruit" + i + "@gmail.com")
+                    .password("Qwerty123")
+                    .build();
+            app.getUser().openRegistrationForm();
+            app.getUser().fillRegistrationForm(user);
+            app.getUser().submitLoginYalla();
+            app.getUser().pause(3000);
+            Assert.assertTrue(app.getUser().isRegistrationUnsuccessful());
+        }
+
+        @Test
+        public void registrationNegativeEmail () {
+            int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+            User user = User.builder()
+                    .name("Peter")
+                    .lastName("Parker")
+                    .email("spiderman" + i + ".com")
+                    .password("Spiderman123$")
+                    .build();
+            app.getUser().openRegistrationForm();
+            app.getUser().fillRegistrationForm(user);
+            app.getUser().submitLoginYalla();
+            app.getUser().pause(3000);
+            Assert.assertTrue(app.getUser().isRegistrationUnsuccessful());
+        }
+
+        @AfterMethod
+        public void postcondition () {
+
+            app.getUser().closePopUpWindow();
+
+
+        }
+
 
     }
-    @Test
-    public void registrationNegativeEmail(){
-        int i = (int)(System.currentTimeMillis()/1000)%3600;
-        User user = User.builder()
-                .name("Peter")
-                .lastName("Parker")
-                .email("spiderman"+ i +".com")
-                .password("Spiderman123$")
-                .build();
-        app.getUser().openRegistrationForm();
-        app.getUser().fillRegistrationForm(user);
-        app.getUser().submitLoginYalla();
-        app.getUser().pause(3000);
-        Assert.assertTrue(app.getUser().isRegistrationUnsuccessful());
-    }
 
-    @AfterMethod
-    public void postcondition(){
-        app.getUser().closePopUpWindow();
-
-    }
-
-
-}
